7a559a9eab08cd6e17d68a47eae317fecc519bf0
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-09-03 03:41:58 +0000

    Commit

--- .devcontainer/01_class/factorial.py
+++ .devcontainer/01_class/factorial.py
@@ -1,8 +1,10 @@
 
-num = 5
-result = 0
+def fact(num):
+    result = 1
+    for i in range (1, num +1):
+        result*=i
+    return result
 
 # for i in 5
-result = result*(num + 1)
-print(result)
-    
+num = (input("Enter the number for Factorial"))
+print(f"The factorial of {num} is {fact(num)} : ")
