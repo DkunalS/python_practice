fd551556a59cecdc17c05c721e3228022ebdf288
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-09-03 03:01:32 +0000

    Commit

--- .devcontainer/01_class/claculator.py
+++ .devcontainer/01_class/claculator.py
@@ -3,12 +3,12 @@ num2 = float(input("Enter the number: "))
 operator = input("Enter the operator: ")
 
 if operator == '+':
-    print(num1 + num2)
+    print("result is: "num1 + num2)
 elif operator == '-':
-    print(num1 - num2)
+    print("result is: "num1 - num2)
 elif operator == '*':
-    print(num1 * num2)
+    print("result is: "num1 * num2)
 elif operator == '/':
-    print(num1 / num2)
+    print("result is: "num1 / num2)
 else:
     print("You have entered wrong opeartion")
